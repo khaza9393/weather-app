18ca1d3da3d6a8d88693aa3c0bcc0ab14ae7b639
khaza9393 <khaza.shaik1431@gmail.com>
2026-06-16 12:53:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const apikey="b7423ffd1c153a01d4885ffa68aaa6c9";
+const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchbox= document.querySelector(".search-input");
+    const searchbtn=document.querySelector(".search-btn");
+const weathericon= document.querySelector(".weather-icon");
+
+
+// const dis=document.querySelector(".description");
+    async function checkweather(city) {
+        const response = await fetch(apiurl+city+`&appid=${apikey}`);
+        const data = await response.json();
+        console.log(data);
+
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temperature").innerHTML= Math.round(data.main.temp)+"°C";
+document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";
+
+if(data.weather[0].main =="clouds"){
+    weathericon.src="cloudy.png";
+    
+}
+else if(data.weather[0].main=="clear"){
+  weathericon.src="sky.png";
+}
+document.querySelector(".description").innerHTML=data.weather[0].description;
+
+
+    }
+   
+
+    
+searchbtn.addEventListener("click",()=>{
+    checkweather(searchbox.value);
+})
+
+
